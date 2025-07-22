ae82826824330dae24752ccc9f59bc34f7288bb3
Md. Shofikul Islam <islamshofi942@gmail.com>
2025-07-22 23:34:57 +0600

    added   empty set

--- set.py
+++ set.py
@@ -1,3 +1,30 @@
-A = {'orange', 'banana', 'pear', 'apple'}
-# print( type(A))
-print(A)
+# A = {'orange', 'banana', 'pear', 'apple'}
+# # print( type(A))
+# print(A)
+
+
+
+
+
+
+
+
+# A = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+# print(A)
+
+
+
+# a=set()
+# print(type(a))
+
+
+
+
+
+# a={}
+# print(type(a))
+
+
+
+
+
